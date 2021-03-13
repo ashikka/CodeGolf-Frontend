2267d64e10e5875cae047b659ff36f3c491468bc
ashikka <ashikagupta28@gmail.com>
2021-03-13 23:27:23 +0530

    feat: Redux integration

--- src/pages/QuestionsPage/QuestionsPage.jsx
+++ src/pages/QuestionsPage/QuestionsPage.jsx
@@ -1,64 +1,39 @@
-/* eslint-disable max-len */
-/* eslint-disable react/prop-types */
-import React from 'react';
-import propTypes from 'prop-types';
+import { React, useEffect } from "react";
+import Question from "../../components/question/question";
+import Footer from "../../components/footer/footer";
+import "./QuestionsPage.css";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchQuestions } from "../../redux/question/questionSlice";
+import ModalBox from "../../components/modal/modal";
+import Leaderboard from "../../components/leaderboard/leaderboard";
 
-import Question from '../../components/question/question';
-import Footer from '../../components/footer/footer';
-import './QuestionsPage.css';
-import ModalBox from '../../components/modal/modal';
-import Leaderboard from '../../components/leaderboard/leaderboard';
+const QuestionsPage = () => {
 
-const QuestionsPage = ({ questions, leaderboards, user }) => (
+  const dispatch = useDispatch();
+  const questions = useSelector((state) => state.question.questions);
+
+  useEffect(() => {
+      dispatch(fetchQuestions());
+  }, [dispatch]);
+  return (
     <>
-        <div>
-            <ModalBox />
-            <div className="content-area">
-                <div className="questions">
-                    <div className="user-name">
-                        {`Welcome, ${user.username}`}
-                    </div>
-                    <div className="question-heading heading">Challenges</div>
-                    <div className="questions-ind">
-                        {questions.map((question) => (
-                            <Question
-                                key={Math.random() * 1000}
-                                question={question}
-                                numberOfSolves={leaderboards.find((leaderboard) => leaderboard.questionName === question.questionName).users.length}
-                            />
-                        ))}
-                    </div>
-                </div>
-                <Leaderboard
-                    leaderboard={leaderboards.find(
-                        (leaderboard) => leaderboard.questionName === 'Global',
-                    )}
-                />
+      <div>
+        <ModalBox />
+        <div className="content-area">
+          <div className="questions">
+            <div className="question-heading heading">Challenges</div>
+            <div className="questions-ind">
+              {questions.map((question) => (
+                <Question key={Math.random() * 1000} question={question} />
+              ))}
             </div>
-            <Footer />
+          </div>
+          <Leaderboard />
         </div>
+        <Footer />
+      </div>
     </>
-);
-
-QuestionsPage.propTypes = {
-    questions: propTypes.arrayOf(
-        propTypes.shape({
-            questionName: propTypes.string.isRequired,
-            points: propTypes.number.isRequired,
-        }),
-    ).isRequired,
-    leaderboard: propTypes.shape({
-        questionName: propTypes.string.isRequired,
-        users: propTypes.arrayOf(
-            propTypes.shape({
-                username: propTypes.string.isRequired,
-                score: propTypes.number.isRequired,
-                questionsSolved: propTypes.number.isRequired,
-                slength: propTypes.number.isRequired,
-                latestTime: propTypes.instanceOf(Date).isRequired,
-            }),
-        ),
-    }).isRequired,
+  );
 };
 
 export default QuestionsPage;
