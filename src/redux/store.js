e4006671bdae3bbcbd5082ea477e32bc519726cb
ashikka <ashikagupta28@gmail.com>
2021-04-08 18:35:38 +0530

    fix: Removed unused reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userSlice from './user/userSlice';
 import questionReducer from './question/questionSlice';
 import leaderboardReducer from './leaderboard/leaderboardSlice';
-import testcaseReducer from './testcase/testcaseSlice';
 
 export default configureStore({
     reducer: {
-        user: userSlice,
         question: questionReducer,
         leaderboard: leaderboardReducer,
-        testcase: testcaseReducer,
     },
 });
